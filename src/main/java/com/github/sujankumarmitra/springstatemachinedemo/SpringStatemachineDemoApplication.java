672a7a0d470da50a781d543afb3b7dff95aa7e66
skmitra <mitrakumarsujan@gmail.com>
2021-01-12 20:02:59 +0530

    Project Init

---
+++ src/main/java/com/github/sujankumarmitra/springstatemachinedemo/SpringStatemachineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.sujankumarmitra.springstatemachinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStatemachineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStatemachineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sujankumarmitra/springstatemachinedemo/SpringStatemachineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sujankumarmitra.springstatemachinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStatemachineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
